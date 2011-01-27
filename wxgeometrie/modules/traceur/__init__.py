wxgeo/geophar

commit 9291aefc9f7e9d69d85db4d787a332dd781fe512

Issue 172: text fields are now always kept synchronised with plots in Traceur module.

wxgeometrie/modules/traceur/__init__.py
@@ -88,6 +88,7 @@ class Traceur(Panel_API_graphique):
         self.sizer.Add(self.canvas, 1, wx.LEFT | wx.TOP | wx.GROW, 0)
         self.sizer.Add(self.entrees, 0, wx.ALL|wx.GROW, 5)
         self.finaliser(contenu = self.sizer)
+        self._changement_feuille()
 
 
     def activer(self):
@@ -96,7 +97,9 @@ class Traceur(Panel_API_graphique):
 
     def _changement_feuille(self):
         u"""Après tout changement de feuille."""
-        self._synchroniser_champs()
+        if hasattr(self, 'nombre_courbes'): # initialisation terminée
+            self._synchroniser_champs()
+            self.feuille_actuelle.lier(self._synchroniser_champs)
 
 
     def _synchroniser_champs(self):

wxgeometrie/geolib/classeur.py
@@ -48,23 +48,21 @@ class Classeur(list):
             list.append(self, feuille)
         else:
             list.insert(self, i + 1, feuille)
-        self.modifie = True
+        self._classeur_modifie()
 
     def _get_actuelle(self):
         return self[-1]
 
     def _set_actuelle(self, feuille):
         self[-1] = feuille
-        if self.parent is not None:
-            self.parent._changement_feuille()
-        self.modifie = True
+        self._classeur_modifie()
 
     def _del_actuelle(self):
         self.pop()
         if not self:
             # Il faut qu'il y ait toujours une feuille pour travailler
             self.nouvelle_feuille()
-        self.modifie = True
+        self._classeur_modifie()
 
     feuille_actuelle = property(_get_actuelle, _set_actuelle, _del_actuelle)
 
@@ -80,8 +78,13 @@ class Classeur(list):
                 i += 1
             nom = "Feuille " + str(i)
         self.append(Feuille(self, titre = nom, log = self.log, parametres = parametres))
-        self.modifie = True
+        self._classeur_modifie()
         return self[-1]
+
+    def _classeur_modifie(self):
+        self.modifie = True
+        if self.parent is not None:
+            self.parent._changement_feuille()
 
     def vider(self):
         while self:
@@ -92,4 +95,3 @@ class Classeur(list):
     @property
     def noms(self):
         return [feuille.nom for feuille in self]
-

wxgeometrie/geolib/feuille.py
@@ -175,7 +175,7 @@ Attributs spéciaux:
         # NB: 'True = True' et 'False = False' : non compatible Py3k
 
         types = {'points': 'Point_generique', 'droites': 'Droite_generique', 'polygones': 'Polygone_generique',
-                 'segments': 'Segment', 'cercles': 'Cercle_generique', 'arcs': 'Arc_generique', 
+                 'segments': 'Segment', 'cercles': 'Cercle_generique', 'arcs': 'Arc_generique',
                  'textes': 'Texte_generique', 'vecteurs': 'Vecteur_generique', 'variables': 'Variable'}
         d = {}
         for typ in types:
@@ -509,6 +509,8 @@ class Interprete_feuille(object):
         self.feuille.canvas.redetecter = True
         if self.feuille.classeur is not None and self.feuille.classeur.parent is not None:
             self.feuille.classeur.parent.rafraichir_titre()
+        for action in self.feuille._actions:
+            action()
 
 
     def parser(self, commande):
@@ -723,12 +725,19 @@ class Feuille(object):
             "notes": "",
             }
 
+        # Actions à effectuer après qu'une commande ait été exécutée.
+        self._actions = []
+
 
 #        Objet.__feuille__ = self # les objets sont crees dans cette feuille par defaut
 
 # ---------------------------------------
 #    Gestion des paramètres du repère
 # ---------------------------------------
+
+    def lier(self, action):
+        if not is_in(action, self._actions):
+            self._actions.append(action)
 
     def affichage_perime(self):
         # NB: Utiliser une méthode au lieu d'un attribut permet de générer
@@ -1295,6 +1304,8 @@ class Feuille(object):
                         print u"Affichage de l'erreur impossible !"
                     self.erreur(u"Chargement incomplet de la feuille.")
                 finally:
+                    for action in self._actions:
+                        action()
                     if archiver:
                         self.historique.archiver()
 

wxgeometrie/geolib/objet.py
@@ -1475,9 +1475,9 @@ class Objet(object):
 
 
     def supprimer(self):
-        if self.style('protege'):
-            self.cacher()
-            return
+#        if self.style('protege'):
+#            self.cacher()
+#            return
         # Le nom doit être récupéré AVANT la suppression.
         nom = self.nom_complet
         if self.protege:

commit 87a9e178bc1517a32bbe3e0248f5f26c69118bff

Issue 180: fix regression introduced by commit 9291aefc9f.
Allow back syntactic sugar to customize curves extremities.
For example, 2;4] means that no symbol must be used on left (2),
but a point must appear on right (4).

wxgeometrie/modules/traceur/__init__.py
@@ -114,12 +114,29 @@ class Traceur(Panel_API_graphique):
         Lors de l'ouverture d'un fichier, ou d'un changement de feuille."""
         for i in xrange(self.nombre_courbes):
             nom_courbe = 'Cf' + str(i + 1)
-##            nom_fonction = 'f' + str(i + 1)
             if self.feuille_actuelle.objets.has_key(nom_courbe):
                 objet = self.feuille_actuelle.objets[nom_courbe]
                 self.boites[i].SetValue(objet.style('visible'))
                 self.equations[i].SetValue(objet.fonction.expression)
-                self.intervalles[i].SetValue(objet.fonction.ensemble)
+                ensemble = objet.fonction.ensemble
+                print 'Ensemble:', ensemble
+                ensemble = re.sub(r"(?<=[][])\+(?=[][])", 'U', ensemble)
+                print 'Ensemble (2):', ensemble
+                extremites_cachees = (str(e) for e in objet.fonction.style('extremites_cachees'))
+                parties = ensemble.split('|')
+
+                j = 0
+                for partie, extremites in zip(parties, extremites_cachees):
+                    print j, partie, extremites
+                    def f(m):
+                        a, b, c, d = m.groups()
+                        return (a if b not in extremites else '') + b + ';' \
+                                + c + (d if c not in extremites else '')
+                    parties[j] = re.sub(r"([][])([^][;]+);([^][;]+)([][])", f, partie)
+                    j += 1
+                print parties
+
+                self.intervalles[i].SetValue('|'.join(parties))
             else:
                 self.boites[i].SetValue(True)
                 self.equations[i].SetValue('')