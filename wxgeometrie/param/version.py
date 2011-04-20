270cf4625801d78a80623ee9827a922d0f319c6a
Nicolas Pourcelot <nicolas.pourcelot@gmail.com>
2011-04-20 10:32:17 +0200

    Version updated. *.tar.gz added to .gitignore.

--- wxgeometrie/param/version.py
+++ wxgeometrie/param/version.py
@@ -2,6 +2,6 @@
 from __future__ import division # 1/2 == .5 (par defaut, 1/2 == 0)
 
 # Numéro de version (et date de sa sortie)
-version = "0.132.4"
+version = "0.132.5"
 # Attention de ne pas faire commencer les nombres par un 0 !
-date_version = (2011, 4, 11)
+date_version = (2011, 4, 20)
