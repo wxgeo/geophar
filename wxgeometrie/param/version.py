ce2175dc22eeca74de2c81b5cc68a91fd0cebfe7
Nicolas Pourcelot <nicolas.pourcelot@gmail.com>
2011-06-03 22:13:05 +0200

    Version 0.132.7

--- wxgeometrie/param/version.py
+++ wxgeometrie/param/version.py
@@ -2,6 +2,6 @@
 from __future__ import division # 1/2 == .5 (par defaut, 1/2 == 0)
 
 # Numéro de version (et date de sa sortie)
-version = "0.132.6"
+version = "0.132.7"
 # Attention de ne pas faire commencer les nombres par un 0 !
-date_version = (2011, 5, 31)
+date_version = (2011, 6, 3)
