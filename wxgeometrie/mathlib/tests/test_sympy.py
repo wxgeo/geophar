0c33455810393d2e6911458744fa97ad0fc62ded
Nicolas Pourcelot <nicolas.pourcelot@gmail.com>
2011-06-21 19:33:46 +0200

    Fix issue 2508 (sympy)

--- wxgeometrie/mathlib/tests/test_sympy.py
+++ wxgeometrie/mathlib/tests/test_sympy.py
@@ -19,10 +19,11 @@ def test_sympy():
     assert (1-sympy.exp(x)).is_negative is None
     assert sympy.Matrix([[1, 2], [3, 4]])**sympy.Integer(2) == sympy.Matrix([[7, 10], [15, 22]])
     assertAlmostEqual(sympy.E._evalf(50), math.e)
-    assert sympy.solve(1/x, x) == []
-    assert sympy.solve(-(1 + x)/(2 + x)**2 + 1/(2 + x), x) == []
+    assert sympy.solve(1/x, x) == [] # issue 1694
+    assert sympy.solve(-(1 + x)/(2 + x)**2 + 1/(2 + x), x) == [] # issue 1694
     assert sympy.limit(1 + 1/x, x, 0, dir='-') == -oo
     assert sympy.limit(1/x**2, x, 0, dir='-') == oo
+    assert sympy.sympify(u'45') == 45 # issue 2508
 
 @XFAIL
 def test_sympy_1_div_0():
