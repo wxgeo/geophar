482a61c2169c97484013c5413ebd87994bd45b07
Nicolas Pourcelot <nicolas.pourcelot@gmail.com>
2012-03-10 23:55:55 +0100

    Version 12.03.2

--- wxgeometrie/param/version.py
+++ wxgeometrie/param/version.py
@@ -2,5 +2,5 @@
 from __future__ import division # 1/2 == .5 (par defaut, 1/2 == 0)
 
 # Numéro de version (et date de sa sortie)
-version = '12.03.1'
-date_version = (2012, 3, 8)
+version = '12.03.2'
+date_version = (2012, 3, 10)
