c69021db54f58c7330691246a6a3185d27662fda
Nicolas Pourcelot <nicolas.pourcelot@gmail.com>
2012-07-15 00:08:39 +0200

    Add an empty `test_widget.py` file (to be filled later...), so unit tests don't complain too much about missing tests.

---
+++ wxgeometrie/geolib/tests/test_widgets.py
@@ -0,0 +1,16 @@
+# -*- coding: iso-8859-1 -*-
+from __future__ import division # 1/2 == .5 (par defaut, 1/2 == 0)
+
+from pytest import XFAIL
+
+from tools.testlib import rand
+from wxgeometrie.geolib import Champ, Bouton
+
+
+@XFAIL
+def test_Champ():
+    raise NotImplementedError
+
+@XFAIL
+def test_Bouton():
+    raise NotImplementedError
