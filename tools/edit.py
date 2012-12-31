77f3158c0ed2f512ac31feaac35941d98afca849
Nicolas Pourcelot <nicolas.pourcelot@gmail.com>
2012-12-31 18:45:48 +0100

    Add support for pyflakes format in `tools/edit.py`.

--- tools/edit.py
+++ tools/edit.py
@@ -43,6 +43,9 @@ def edit(file_and_line, editor=DEFAULT_EDITOR):
     file_and_line = file_and_line.strip()
     try:
         m = re.search('File "?([^"]+[.]py)"?, line ([0-9]+)', file_and_line)
+        if m is None:
+            # Format pyflakes
+            m = re.search('([^"]+[.]py):([0-9]+)', file_and_line)
         file_name = m.group(1)
         line = m.group(2)
     except AttributeError:
