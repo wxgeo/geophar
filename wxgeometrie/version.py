37300901c922905bb6a170b3486bf0e009fdad58
Nicolas Pourcelot <nicolas.pourcelot@gmail.com>
2013-06-05 14:13:33 +0200

    Version 13.04.7

--- wxgeometrie/version.py
+++ wxgeometrie/version.py
@@ -5,6 +5,6 @@ NOMPROG = u"Géophar"
 NOMPROG2 = 'Geophar' # sans accent
 
 # Numéro de version (et date de sa sortie)
-version = '13.04.6'
-date_version = (2013, 5, 15)
-git = 'v13.04.5-9-gcbff610\n'
+version = '13.04.7'
+date_version = (2013, 6, 5)
+git = 'v13.04.6-7-g2a24091\n'
