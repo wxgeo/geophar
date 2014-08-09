9c91e8080d21437c83c99c9935b8aa395c99c0dc
Nicolas Pourcelot <nicolas.pourcelot@gmail.com>
2014-08-09 23:34:47 +0200

    Tikz backend implementation has no enough substance to be usable.
    Still much remains to be done for a full implementation.

--- wxgeometrie/geolib/backends/backend_tikz.py
+++ wxgeometrie/geolib/backends/backend_tikz.py
@@ -24,25 +24,94 @@ from __future__ import with_statement
 #    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 from math import pi
+from re import sub, compile
+
+from ...mathlib.intervalles import conversion_chaine_ensemble
+from ...mathlib.parsers import traduire_formule, NBR_SIGNE_OR_VAR
 
 
+# \usetikzlibrary{arrows.meta}
 
 class BackendTikz(object):
 
+    _arrow = '-{Stealth[scale=1.7]}'
+
+    _dict_styles_traits = {':': 'dotted', '--': 'dashed', '-': 'solid'}
+
+    _dict_couleurs = {'g': 'green', 'r': 'red', 'b': 'blue', 'y': 'yellow',
+                     'k': 'black', 'c': 'cyan', 'm': 'magenta', 'w': 'white'}
+
+    def _style_trait(self, style):
+        return self._dict_styles_traits.get(style, '')
+
+    def _epaisseur(self, value):
+        u'Convertit la largeur de ligne.'
+        return str(value/2) + 'pt'
+
+    def _couleur(self, couleur):
+        if isinstance(couleur, tuple):
+            r, g, b, a = couleur
+            return '{rgb:red,%s;green,%s;blue,%s}' % (r, g, b)
+        else:
+            return self._dict_couleurs.get(couleur, couleur)
+
+    def _add(self, commande):
+        self._lignes.append(commande)
+
+    def _draw(self, path, options=[], arrow=False, node='', **style):
+        options = options[:] # Attention à bien recréer une nouvelle liste !
+        if isinstance(path, (list, tuple)):
+            path = ' -- '.join('(%s, %s)' % (x, y) for x, y in path)
+        if 'couleur' in style:
+            options.append('color=' + self._couleur(style['couleur']))
+        if 'style' in style:
+            options.append(self._style_trait(style['style']))
+        if arrow:
+            options.append(self._arrow)
+        if node:
+            node = ' node ' + node
+        self._add(r'\draw[%s] %s%s;' % (', '.join(filter(None, options)), path, node))
+
     def exporter(self, feuille, **options):
-        commands = [r'\begin{tikzpicture}']
-        commands.append(self._export_repere(feuille))
+        self._lignes = [r'\begin{tikzpicture}']
+        self._export_repere(feuille)
         for objet in feuille.liste_objets(objets_caches=False, etiquettes=False):
             for classe in objet.__class__.__mro__:
                 methode = '_export_' + classe.__name__
                 if hasattr(self, methode):
-                    commands.append(getattr(self, methode)(objet, **options))
+                    getattr(self, methode)(objet, **options)
                     break
-        return '\n'.join(commands + [r'\end{tikzpicture}'])
+        self._lignes.append(r'\end{tikzpicture}')
+        return '\n'.join(self._lignes)
 
     def _export_repere(self, feuille, **options):
         xmin, xmax, ymin, ymax = feuille.fenetre_reellement_affichee()
-        return r'\clip (%s, %s) rectangle (%s, %s);' % (xmin, ymin, xmax, ymax)
+        xgradu, ygradu = feuille.gradu
+        origine, abscisse, ordonnee = feuille.repere
+        self._add(r'\clip (%s, %s) rectangle (%s, %s);' % (xmin, ymin, xmax, ymax))
+
+        if feuille.afficher_quadrillage:
+            # TODO: style, epaisseur, couleur
+            # Créer des dictionnaires convertir style et epaisseur.
+
+            for (xpas, ypas), style, epaisseur, couleur in feuille.quadrillages:
+                xpas = xpas or xgradu
+                ypas = ypas or ygradu
+                self._draw('(%s, %s) grid (%s, %s)' % (xmin, ymin, xmax, ymax),
+                            options=['xstep=%s' % xpas, 'ystep=%s' % ypas,
+                                     self._style_trait(style),
+                                    'color=%s' % self._couleur(couleur),
+                                    'line width=%s' % self._epaisseur(epaisseur)])
+        if feuille.afficher_axes:
+            self._draw([(xmin, 0), (xmax, 0)], arrow=True)
+            self._draw([(0, ymin), (0, ymax)], arrow=True)
+            # TODO: support des vecteurs i et j
+            options = ['thick']
+            self._draw('(0, 0) node[below left] {$%s$}' % origine, options)
+            self._draw('(%s, 2pt) -- (%s, -2pt)' % (xgradu, xgradu), options,
+                        node='[below] {$%s$}' % abscisse)
+            self._draw('(2pt, %s) -- (-2pt, %s)' % (ygradu, ygradu), options,
+                        node='[left] {$%s$}' % ordonnee)
 
     def _export_Objet(self, objet, **options):
         print(u"Warning: type d'objet non supporté pour l'instant : "
@@ -53,35 +122,67 @@ class BackendTikz(object):
         # 1.8 : coefficient purement empirique...!
         r = 1.8*P.etiquette.style('_rayon_')
         a = P.etiquette.style('_angle_')*180/pi
-        return (r'\draw[auto=right] (%s, %s) node (%s) {\small $\bullet$};' '\n'
-                r'\path (%s) ++(%s:%s pt) node (%s_etiquette) {%s};'
-                % (P.x, P.y, P.nom,
-                   P.nom, a, r, P.nom, P.label()))
+        # TODO: gérer style du point
+        self._draw([P], node=r'(%s) {\small $\bullet$}' % P.nom)
+        self._add(r'\path (%s) ++(%s:%s pt) node (%s_etiquette) {%s};'
+                    % (P.nom, a, r, P.nom, P.label()))
         #~ \path (A) ++(30:2) node (B) [draw,fill=blue!20] {B};
         #~ \node at (1,0) [label={[shift={(1.0,0.3)}]label}] {Node};
 
     def _export_Texte_generique(self, txt, **options):
-        return r'\draw (%s, %s) node {%s};' % (txt.x, txt.y, txt.label())
+        self._draw([txt], node='{%s}' % txt.label())
 
     def _export_Cercle_generique(self, cercle, **options):
-        x, y = cercle.centre.xy
-        return r'\draw (%s, %s) circle (%s);' % (x, y, cercle.rayon)
+        x, y = cercle.centre
+        self._draw('(%s, %s) circle (%s)' % (x, y, cercle.rayon), **cercle.style())
 
     def _export_Polygone_generique(self, poly, **options):
-        l = ['(%s, %s)' % (P.x, P.y) for P in poly.points]
-        return r'\draw %s;' % ' -- '.join(l)
+        self._draw(poly.points, **poly.style())
 
     def _export_Segment(self, segment, **options):
-        A, B = segment.extremites
-        return r'\draw (%s, %s) -- (%s, %s);' % (A.x, A.y, B.x, B.y)
+        self._draw(segment.extremites, **segment.style())
 
     def _export_Droite(self, droite, **options):
         points = droite._points_extremes()
-        if len(points) < 2:
-            # La droite ne coupe pas la fenêtre (ou seulement en un point)
-            return ''
-        (x1, y1), (x2, y2) = points
-        return r'\draw (%s, %s) -- (%s, %s);' % (x1, y1, x2, y2)
+        if len(points) == 2:
+            # Sinon, la droite ne coupe pas la fenêtre (ou seulement en un point)
+            self._draw(points, **droite.style())
 
     def _export_Courbe(self, courbe, **options):
-        return ''
+        # TODO: gérer les extrémités de courbe (points, arc de cercle)
+        xmin, xmax, ymin, ymax = courbe.feuille.fenetre_reellement_affichee()
+        fonction = courbe.fonction
+        x = fonction.variable
+        liste_fx = fonction.expression.split('|')
+        liste_Df = fonction.ensemble.split('|')
+
+        RE_VAR = compile(r'(?<!\w)(%s)(?!\w)' % x)
+        x_ = '\\' + x
+        for (fx, Df) in zip(liste_fx, liste_Df):
+            Df = conversion_chaine_ensemble(Df)
+            fx = traduire_formule(fx)
+            if '**' in fx or '^' in fx:
+                # TODO: gérer les parenthèses imbriquées : (x(x+1))**2
+                def conv(m):
+                    return 'pow(%s, %s)' % (m.group('a') or m.group('A'), m.group('b') or m.group('B'))
+                RE_POW = r'((?P<a>%s)|(?P<A>\([^)]+\)))(\^|\*\*)((?P<b>%s)|\((?P<B>[^)]+)\))' \
+                        % (NBR_SIGNE_OR_VAR, NBR_SIGNE_OR_VAR)
+                fx = sub(RE_POW, conv, fx)
+            fx = sub(RE_VAR, x_, fx)
+            for I in Df.intervalles:
+                xm = float(max(xmin, I.inf))
+                xM = float(min(xmax, I.sup))
+                self._draw(r'plot ({%s}, {%s})' % (x_, fx),
+                                options=['domain=%s:%s,smooth,samples=500,variable=%s' % (xm, xM, x_)],
+                                **courbe.style())
+            for val, symb in Df.extremites(xmin, xmax):
+                if symb == '.':
+                    self._draw(r'(%s, %s) node {\small$\bullet$}', **courbe.style())
+                else:
+                    print(u"Warning: extrémité de courbe non générée\n"
+                          u"(extrémité de type %s non géré pour l'instant)." % repr(symb))
+
+    def _export_Vecteur(self, vecteur, **options):
+        A, B = vecteur.extremites
+        self._draw(vecteur.extremites, node='[midway] {%s}' % vecteur.label(),
+                  options=['auto=right'], arrow=True, **vecteur.style())
