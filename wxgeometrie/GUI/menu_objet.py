wxgeo/geophar

commit a827e2c1be48180ed9cf3a0c4fccec7b367a3ac7

[FS#226] Replace unpythonic int constants (like NOM) by strings ('nom' here).

wxgeometrie/GUI/menu_objet.py
@@ -27,7 +27,6 @@ from PyQt4.QtGui import (QDialog, QVBoxLayout, QHBoxLayout, QFrame, QLineEdit,
                          QMenu,)
 from PyQt4.QtCore import Qt
 
-from ..geolib.constantes import NOM, FORMULE, TEXTE, RIEN, MATH
 from ..geolib import Texte_generique, Point_generique, Champ, Texte, Polygone_generique
 from .proprietes_objets import Proprietes
 from ..pylib import print_error
@@ -63,7 +62,7 @@ class MenuActionsObjet(PopUpMenu):
         if isinstance(select, Texte_generique):
             action = self.addAction(u"Éditer le texte")
             action.triggered.connect(self.etiquette)
-            action = self.addAction(u"Formatage mathématique" if select.style('formatage') == RIEN
+            action = self.addAction(u"Formatage mathématique" if select.style('formatage') == 'rien'
                                             else u"Formatage par défaut")
             action.triggered.connect(self.mode_formatage)
 
@@ -179,7 +178,7 @@ class MenuActionsObjet(PopUpMenu):
         sizer.addWidget(dlg.text)
 
         dlg.cb = QCheckBox(u"Interpréter la formule", dlg)
-        dlg.cb.setChecked(select.mode_affichage == FORMULE)
+        dlg.cb.setChecked(select.mode_affichage == 'formule')
         sizer.addWidget(dlg.cb)
 
         line = QFrame(self)
@@ -205,7 +204,7 @@ class MenuActionsObjet(PopUpMenu):
                 try:
                     nom = select.nom
                     txt = repr(dlg.text.toPlainText())
-                    mode = (FORMULE if dlg.cb.isChecked() else TEXTE)
+                    mode = ('formule' if dlg.cb.isChecked() else 'texte')
                     self.executer(u"%s.label(%s, %s)" %(nom, txt, mode))
                 except:
                     # Au cas où une formule incorrecte fasse buguer l'affichage (?)
@@ -245,18 +244,18 @@ class MenuActionsObjet(PopUpMenu):
     def masquer_nom(self):
         select = self.canvas.select
         if select.label():
-            mode = RIEN
+            mode = 'rien'
         else:
             if select.legende:
-                mode = TEXTE
+                mode = 'texte'
             else:
-                mode = NOM
+                mode = 'nom'
         self.executer(u"%s.label(mode = %s)" %(select.nom, mode))
 
     def mode_formatage(self):
         select = self.canvas.select
         actuel = select.style('formatage')
-        select.style(formatage=(MATH if actuel == RIEN else RIEN))
+        select.style(formatage=('math' if actuel == 'rien' else 'rien'))
 
     def redefinir(self):
         u"""Redéfinit l'objet (si possible).

wxgeometrie/API/filtres.py
@@ -48,8 +48,9 @@ def filtre_versions_anterieures(fgeo, version):
             # 0.108
             if version < [0, 108]:
                 lignes = figures[i].split("\n")
+                reg = re.compile(u"[A-Za-z_][A-Za-z_0-9]*[=](Point|Intersection|Glisseur|Projete|Barycentre|Milieu|Centre|Orthocentre)")
                 for j in range(len(lignes)):
-                    if not re.match(u"[A-Za-z_][A-Za-z_0-9]*[=](Point|Intersection|Glisseur|Projete|Barycentre|Milieu|Centre|Orthocentre)", lignes[j]):
+                    if not re.match(reg, lignes[j]):
                         lignes[j] = lignes[j].replace(u"'legende': 1", u"'legende': 2")
                 figures[i] = "\n".join(lignes)
 
@@ -121,7 +122,10 @@ def filtre_versions_anterieures(fgeo, version):
             # 0.123.1
             # Les noms Cercle et Cercle_rayon permutent.
             if version < [0, 123, 1]:
-                figures[i] = figures[i].replace("Cercle_rayon(", "_Gtftqsff45ezytaezfehge(").replace("Cercle(", "Cercle_rayon(").replace("_Gtftqsff45ezytaezfehge(", "Cercle(")
+                randstr = "_Gtftqsff45ezytaezfehge("
+                figures[i] = figures[i].replace("Cercle_rayon(", randstr)\
+                                       .replace("Cercle(", "Cercle_rayon(")\
+                                       .replace(randstr, "Cercle(")
 
             # version 0.124
             # Nouveau codage des ' et autres " dans les noms d'objets
@@ -266,6 +270,13 @@ def filtre_versions_anterieures(fgeo, version):
 
             if [13] <= version < [14, 5]:
                 figures[i] = figures[i].replace("'fond': 'none'", "'fond': False")
+            if version < [14, 8]:
+                for old, new in ((0, 'rien'), (1, 'nom'), (2, 'texte'),
+                                 (3, 'formule'), (4, 'math')):
+                    figures[i] = figures[i].replace(", 'mode': %s," % old,
+                                                    ", 'mode': %s," % repr(new))
+                    figures[i] = figures[i].replace(".style(mode = %s)" % old,
+                                                    ".style(mode = %s)" % repr(new))
 
     if version < [13, 1]:
         if fgeo.contenu.has_key("Diagramme") and fgeo.module == "statistiques":

wxgeometrie/GUI/proprietes_objets.py
@@ -38,7 +38,6 @@ from .qtlib import ColorSelecter
 from .app import white_palette, app
 from .. import param
 from ..pylib import print_error, debug, advanced_split, OrderedDict
-from ..geolib.constantes import NOM, FORMULE, TEXTE, RIEN
 from ..geolib.routines import nice_display
 
 class ProprietesAffichage(QWidget):
@@ -83,10 +82,10 @@ class ProprietesAffichage(QWidget):
                 mode = objets[0].mode_affichage
                 legende = QHBoxLayout()
                 self.radios = OrderedDict((
-                        (NOM, QRadioButton("Nom")),
-                        (TEXTE, QRadioButton(u"Texte")),
-                        (FORMULE, QRadioButton(u"Formule")),
-                        (RIEN, QRadioButton(u"Aucun")),
+                        ('nom', QRadioButton("Nom")),
+                        ('texte', QRadioButton(u"Texte")),
+                        ('formule', QRadioButton(u"Formule")),
+                        ('rien', QRadioButton(u"Aucun")),
                               ))
                 if all(objet.mode_affichage == mode for objet in objets):
                     self.radios[mode].setChecked(True)
@@ -484,8 +483,8 @@ class ProprietesAffichage(QWidget):
                     if mode is not None or label is not None:
                         if mode is None:
                             # Conserver le mode formule, sinon basculer en mode TEXTE.
-                            mode = (FORMULE if objet.mode_affichage == FORMULE else TEXTE)
-                        self.canvas.executer(u"%s.label(%s, %s)" %(objet.nom, repr(label), mode))
+                            mode = ('formule' if objet.mode_affichage == 'formule' else 'texte')
+                        self.canvas.executer(u"%s.label(%s, %s)" %(objet.nom, repr(label), repr(mode)))
                         self.radios[mode].setChecked(True)
                         # Le texte a pu changer (ajout automatique des accolades en mode formule)
                         self.etiquette.setText(objet.legende)

wxgeometrie/geolib/__init__.py
@@ -28,7 +28,6 @@ from .cercles import (Cercle_Arc_generique, Arc_generique, Arc_cercle, Arc_point
                        Disque
                        )
 from .classeur import Classeur
-from .constantes import RIEN, TEXTE, NOM, FORMULE
 from .contexte import Contexte, contexte
 from .courbes import Courbe_generique, Courbe
 from .feuille import (MethodesObjets, Liste_objets, ModeTolerant,

wxgeometrie/geolib/constantes.py
@@ -1,31 +1,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import division # 1/2 == .5 (par defaut, 1/2 == 0)
-
-
-##--------------------------------------#######
-#                  constantes                 #
-##--------------------------------------#######
-#    WxGeometrie
-#    Dynamic geometry, graph plotter, and more for french mathematic teachers.
-#    Copyright (C) 2005-2013  Nicolas Pourcelot
-#
-#    This program is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 2 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program; if not, write to the Free Software
-#    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
-
-NONE = RIEN = 0
-NAME = NOM = 1
-TEXT = TEXTE = 2
-FORMULE = 3
-MATH = 4

wxgeometrie/geolib/feuille.py
@@ -57,7 +57,6 @@ from .textes import Texte, Texte_generique
 from .vecteurs import Vecteur_libre
 from .variables import Variable, XMinVar, XMaxVar, YMinVar, YMaxVar, Dpx, Dpy, \
                        Variable_affichage, Pixel_unite
-from .constantes import NOM, RIEN#, FORMULE,
 
 from .pseudo_canvas import _pseudocanvas
 from .. import param
@@ -435,8 +434,8 @@ class Dictionnaire_objets(dict):
             # Attention, la feuille doit être déjà definie !
             nom = valeur._nom_alea()
             # Pour les objets nommés automatiquement, le nom n'est pas affiché par défaut.
-            if valeur.mode_affichage == NOM:
-                valeur.label(mode = RIEN)
+            if valeur.mode_affichage == 'nom':
+                valeur.label(mode='rien')
 
         # Les objets dont le nom commence par "_" ne sont pas affichés par défaut (pure convention) :
         if nom[0] == "_":

wxgeometrie/geolib/labels.py
@@ -28,7 +28,6 @@ from math import cos, sin, hypot, pi, acos
 
 from .objet import Objet, Argument, Ref
 from .textes import Texte_editable_generique, _get_texte, _set_texte
-from .constantes import NOM
 from .routines import angle_vectoriel, sign
 from .points import (Glisseur_arc_cercle, Glisseur_segment, Glisseur_droite,
                      Glisseur_vecteur, Glisseur_cercle, Glisseur_demidroite,)
@@ -169,7 +168,7 @@ class Label_generique(Texte_editable_generique):
 class Label_point(Label_generique):
     u"L'étiquette d'un point."
 
-    _style_defaut = {'mode': NOM}
+    _style_defaut = {'mode': 'nom'}
 
     __parent = parent = Argument('Point_generique')
     __texte = texte = Argument("unicode", _get_texte, _set_texte)

wxgeometrie/geolib/objet.py
@@ -36,7 +36,6 @@ from ..pylib import property2, uu, str2, print_error, \
                     is_in, WeakList, CustomWeakKeyDictionary, warning
 from ..mathlib.parsers import mathtext_parser
 from .routines import nice_display
-from .constantes import NOM, RIEN#, FORMULE, TEXTE,
 ##from .formules import Formule
 from .contexte import contexte
 from .. import param
@@ -860,7 +859,7 @@ class Objet(object):
             >>> D1.label()
             ''
 
-            >>> D1.label(mode=NOM)
+            >>> D1.label(mode='nom')
             >>> print(D1.label())
             $\\mathscr{D}_{1}$
 
@@ -869,10 +868,10 @@ class Objet(object):
             Je suis une droite.
 
         Si le texte contient des formules entre accolades, elles
-        peuvent également être interprétées, si le mode FORMULE
+        peuvent également être interprétées, si le mode 'formule'
         est activé::
 
-            >>> D1.label("A a pour abscisse {A.x}.", mode=FORMULE)
+            >>> D1.label("A a pour abscisse {A.x}.", mode='formule')
             >>> D1.label()
             u'A a pour abscisse -2,7.'
 
@@ -985,9 +984,9 @@ class Objet(object):
             self.feuille.objets._dereferencer(self)
             self.feuille.objets[nom] = self
         if afficher_nom:
-            self.label(mode=NOM)
+            self.label(mode='nom')
         elif afficher_nom is False:
-            self.label(mode=RIEN)
+            self.label(mode='rien')
         self.style(**kw)
         self.figure_perimee()
 

commit 6a84f38338aa4d4be0a7d0747e504671fbbf1b99

Fix regression introduced by commit a827e2c1: can't display object label.

wxgeometrie/GUI/menu_objet.py
@@ -250,7 +250,7 @@ class MenuActionsObjet(PopUpMenu):
                 mode = 'texte'
             else:
                 mode = 'nom'
-        self.executer(u"%s.label(mode = %s)" %(select.nom, mode))
+        self.executer(u"%s.label(mode = %r)" % (select.nom, mode))
 
     def mode_formatage(self):
         select = self.canvas.select