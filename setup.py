2c3a025916495c6247435f692e172093370287c3
Nicolas Pourcelot <nicolas.pourcelot@gmail.com>
2014-08-22 08:36:10 +0200

    Add customized tools/isympy and setup.py.

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from wxgeometrie.param import version
+
+long_description = u"""\
+Geophar provides various tools for math teachers :
+ * dynamic geometry
+ * various plots (function curves, histograms, probability trees...)
+ * symbolic calculus (with extensive LaTeX support)
+ * tools to generate LaTeX code (functions variations and sign tables).
+
+It follows french conventions concerning graphics, as taught in french schools.
+
+Note that Geophar is only avalaible in french currently.
+"""
+
+setup(name=u'Geophar',
+    version=version,
+    description='Dynamic geometry, plots and symbolic calculus for teachers.',
+    long_description=long_description,
+    keywords=['math', 'CAS', 'geometry', 'french', 'latex', 'plot'],
+    author='Nicolas Pourcelot',
+    author_email='nicolas.pourcelot@gmail.com',
+    url='http://wxgeo.free.fr/',
+    license='GPLv2 or next',
+    packages=['wxgeometrie'],
+    package_data = { 'wxgeometrie' : ['wxgeometrie/images/*.png'] },
+    )
