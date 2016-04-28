0ec668bfb00a65d4061d17573cc2effbef0db52a
Nicolas Pourcelot <nicolas.pourcelot@gmail.com>
2016-04-28 14:01:04 +0200

    Improve debuging messages in testlib.

--- tools/testlib.py
+++ tools/testlib.py
@@ -46,7 +46,17 @@ def assertNotAlmostEqual(x, y):
     assert TEST
 
 def assertEqual(x, y):
+    def yellow(s):
+        return '\033[0;33m' + s + '\033[0m'
     if x != y:
+        rx = repr(x)
+        ry = repr(y)
+        if isinstance(x, basestring) and isinstance(y, basestring):
+            for i in range(min(len(rx), len(ry))):
+                if rx[i] != ry[i]:
+                    break
+            ry = ry[:i] + yellow(ry[i:])
+
         print('''
 --------------
  *** FAIL ***
@@ -55,7 +65,7 @@ def assertEqual(x, y):
 -> Expected:
 %s
 --------------
-''' %(repr(x), repr(y)))
+''' %(rx, ry))
     assert (x == y)
 
 assertEq = assertEqual
