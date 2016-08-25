e8f41c451142dcb550e6e9479da268a9914cf80d
Nicolas Pourcelot <nicolas.pourcelot@gmail.com>
2016-08-25 13:38:44 +0200

    Version 16.08.2

--- wxgeometrie/version.py
+++ wxgeometrie/version.py
@@ -5,6 +5,6 @@ NOMPROG = u"Géophar"
 NOMPROG2 = 'Geophar' # sans accent
 
 # Numéro de version (et date de sa sortie)
-version = '16.08.1'
-date_version = (2016, 8, 24)
-git = 'v16.08-3-g3f8144a'
+version = '16.08.2'
+date_version = (2016, 8, 25)
+git = 'v16.08.1-2-gaeb0f9d'
