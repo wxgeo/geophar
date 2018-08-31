478e36c3ad650e0f43ae5607b7e61487916e110a
Nicolas Pourcelot <nicolas.pourcelot@gmail.com>
2018-08-31 15:27:49 +0200

    Version 18.08

--- wxgeometrie/version.py
+++ wxgeometrie/version.py
@@ -4,6 +4,6 @@ NOMPROG = "Géophar"
 NOMPROG2 = 'Geophar' # sans accent
 
 # Numéro de version (et date de sa sortie)
-version = '16.08.4'
-date_version = (2016, 11, 6)
-git = 'v16.08.3-2-gd654f7a'
+version = '18.08'
+date_version = (2018, 8, 31)
+git = 'v16.08.4-87-g736ef86'
